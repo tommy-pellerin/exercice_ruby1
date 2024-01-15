c4570296a7b0caffa2b4ac5fd6e7cce07b290dad
tommy-pellerin <tommytp@hotmail.fr>
2024-01-15 15:26:47 +0100

    ajout de fichiers

---
+++ exo_10.rb
@@ -0,0 +1,3 @@
+puts "Entrer votre annéee de naissance :"
+birth_year=gets.chomp.to_i
+puts "En 2017, vous aviez #{2017-birth_year} ans"
---
+++ exo_11.rb
@@ -0,0 +1,5 @@
+puts "Rentrez un chiffre :"
+user_number = gets.chomp.to_i
+user_number.times do
+  puts "Salut, ça farte ?"
+end
